ec609a34deb54ea204619fc4f37f067cf2a3d0ba
Calin Mihai <32879010+CalinMihai@users.noreply.github.com>
2017-11-14 23:29:32 +0200

    Update I.1.c

--- I.1.c
+++ I.1.c
@@ -1,18 +1,19 @@
-
 #include <stdio.h>
 int n, average[100], i;
 int main(){
     printf("n=");
     scanf("%d", &n);
 if(n >= 3){
-    for(i = 0; i < n; i++){
+    for(i = 1; i < n; i++){
+        scanf("%d", &average[i-1]);
         scanf("%d", &average[i]);
-        average[i] = average[i-1] + average[i+1];
+        scanf("%d", &average[i+1]);
+            average[i] = (average[i-1] + average[i+1])/2;
         printf("%d", average[i]);
                 }
 }
 else
-    printf("n is invallid");
+    printf("n is invalid");
 
 
 }
